efe3358c3d2704f47153e1c92f96f76e4aed299e
Melvin Quashie <melvinquashie@outlook.com>
2023-02-16 20:51:35 +0000

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
+/*
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
+}
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
