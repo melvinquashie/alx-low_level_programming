65c29dfaef2148838a05eb5876d9e933230ea16b
Melvin Quashie <melvinquashie@outlook.com>
2023-02-17 20:51:57 +0000

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,17 +10,19 @@
 
 int main(void)
 {
-	char c;
-  char d;
+char c;
+char d;
 
-	for (c = 'a' ; c <= 'z' ; c++)
-	{
-	putchar(c);
-	}
-  for (d = 'A' ; d <= 'Z' ; d++)
-  {
-  putchar(d);
-  }
+for (c = 'a' ; c <= 'z' ; c++)
+{
+putchar(c);
+}
+
+for (d = 'A' ; d <= 'Z' ; d++)
+{
+putchar(d);
+}
+
 	putchar('\n');
 	return (0);
 }
