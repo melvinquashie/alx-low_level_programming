6a7e06ac12f61cd3ff2bc5af71e3773a66c2c46b
Melvin Quashie <melvinquashie@outlook.com>
2023-02-21 19:38:51 +0000

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,24 @@
-#include <main.h>
+#include "main.h"
+
+/**
+ * Main: Entry point
+ *
+ * Description : prints _putchar and a new line
+ *
+ * Return : 0 (always successful)
+*/
 
 int main(void)
 {
-  puts("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
