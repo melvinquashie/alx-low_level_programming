d62d65b310838c6089465a32ea4cc2f438bcf680
Melvin Quashie <melvinquashie@outlook.com>
2023-02-21 21:26:12 +0000

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,7 @@
  * jack_bauer - prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
  *
- * where h is for the hours and m is for the minutes 
+ * where h is for the hours and m is for the minutes
  */
 void jack_bauer(void)
 {
