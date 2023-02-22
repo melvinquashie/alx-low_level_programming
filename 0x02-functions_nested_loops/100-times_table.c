040167039088a2bfb96ea76c62e21718c959f709
Melvin Quashie <melvinquashie@outlook.com>
2023-02-22 05:24:27 +0000

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -40,7 +40,6 @@ else if (product > 99 && product < 1000)
 {
 _putchar(',');
 _putchar(' ');
-_putchar(' ');
 _putchar(product / 100 + '0');
 _putchar((product / 10) % 10 + '0');
 _putchar(product % 10 + '0');
