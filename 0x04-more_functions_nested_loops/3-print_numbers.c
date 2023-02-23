b07aef65688ee54365930627f4f7fd5e14cca864
Melvin Quashie <melvinquashie@outlook.com>
2023-02-23 20:44:09 +0000

    print numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9
+ * followed by a new line
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,7 +1,7 @@
 0-checks for uppercase letter
 1-checks for a digit, 0 to 9
 2-multiplies 2 integers
-3-
+3-prints numbers from 0-9
 4-
 5-
 6-
