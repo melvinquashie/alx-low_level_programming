a2126de75cc0e4651567dbb4565da4ffd266ef58
Melvin Quashie <melvinquashie@outlook.com>
2023-02-27 21:29:45 +0000

    prints every other

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * followed by a new line
+ *
+ * @str: string to test
+ */
+
+void puts2(char *str)
+{
+int i, j;
+
+
+
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; j < i; j += 2)
+{
+_putchar(str[j]);
+}
+
+}
+
+_putchar('\n');
+}
