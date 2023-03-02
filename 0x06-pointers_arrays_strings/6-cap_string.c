31d15a00712202f055f7a9368af1dcedf895b49f
Melvin Quashie <melvinquashie@outlook.com>
2023-03-02 05:54:32 +0000

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,7 @@ char *cap_string(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] = s[i] -32;
+			s[i] = s[i] - 32;
 
 		for (j = 0; j < 13; j++)
 		{
@@ -25,7 +25,7 @@ char *cap_string(char *s)
 			{
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				{
-					s[i + 1] = s[i+1] - 32;
+					s[i + 1] = s[i + 1] - 32;
 				}
 			}
 		}
