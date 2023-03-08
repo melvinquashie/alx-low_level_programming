8a5746e9c7ad290578d4497d9ffff8fb73f5515c
Melvin Quashie <melvinquashie@outlook.com>
2023-03-08 21:33:08 +0000

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,7 +3,7 @@
  * wildcmp - compares two strings and returns 1
  * if the strings can be considered identical,
  * otherwise return 0.
- * 
+ *
  * @s1: the normal string
  * @s2: the special string that can contain "*"
  *
