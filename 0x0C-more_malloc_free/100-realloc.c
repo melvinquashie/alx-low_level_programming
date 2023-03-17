8700df4d476df4b0e8be564641fbe597e94c7fe9
Melvin Quashie <melvinquashie@outlook.com>
2023-03-17 05:43:58 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,11 +26,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	if (new_size == old_size)
-  {
+{
 		return (ptr);
-  }
+}
 
-  free(ptr);
+free(ptr);
 	ptr = malloc(new_size);
 
 	return (ptr);
