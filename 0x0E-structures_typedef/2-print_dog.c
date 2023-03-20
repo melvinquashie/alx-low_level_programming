f5647bfcb0701339b163426313dbbca462796752
Melvin Quashie <melvinquashie@outlook.com>
2023-03-20 20:55:07 +0000

    working with the dog struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,9 +14,9 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-		d->name = "\0";
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		d->owner = "\0";
+		d->owner = "(nil)";
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
