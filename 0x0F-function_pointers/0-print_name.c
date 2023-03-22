8557c2209b61b022bff52b2d2bbf58e5537107e2
Melvin Quashie <melvinquashie@outlook.com>
2023-03-22 19:47:44 +0000

    code for function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,13 +6,13 @@
  *
  * @name: name to print
  * @f: pointer to the printing function
- * 
+ *
  * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
+	if (f == NULL)
 		return;
 	f(name);
 }
