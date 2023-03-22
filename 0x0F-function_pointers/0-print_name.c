56fcf7905e9b1e8b77a818ada89070d20971447e
Melvin Quashie <melvinquashie@outlook.com>
2023-03-22 20:12:07 +0000

    print name file collection

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,10 +2,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
+ * print_name - prints a name.
  *
- * @name: name to print
- * @f: pointer to the printing function
+ * @name: name to print.
+ * @f: pointer to the printing function.
  */
 
 void print_name(char *name, void (*f)(char *))
