15159220366a4da54fd30ffdde341c1ab5c70d8a
Melvin Quashie <melvinquashie@outlook.com>
2023-04-03 19:09:54 +0000

    free listint_t

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ *
+ * @head: head of linked list
+ *
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp = head;
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(head);
+		head = temp;
+	}
+}
