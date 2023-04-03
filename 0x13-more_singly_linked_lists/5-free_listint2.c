766439eace82b8b43fbdf00ca68d886dbc648613
Melvin Quashie <melvinquashie@outlook.com>
2023-04-03 19:30:37 +0000

    free listint_t catering for head == NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,22 +2,24 @@
 
 /**
  * free_listint2 - frees a listint_t list
+ *
  * @head: head of linked list
+ *
  * Return: void
  */
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *ptr = *head;
 
 	if (head == NULL)
 		return;
 
-	while (*head)
+	while (ptr != NULL)
 	{
-		temp = temp->next;
+		ptr = ptr->next;
 		free(*head);
-		*head = temp;
+		*head = ptr;
 	}
 	*head = NULL;
 }
