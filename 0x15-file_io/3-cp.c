a97fccfafa83b118ce909d9c9891b756d315c0ec
Melvin Quashie <melvinquashie@outlook.com>
2023-04-11 05:25:55 +0000

    task 3  correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,56 +20,40 @@
 int main(int argc, char *argv[])
 {
 int file_from, file_to, r, w;
-/**
- * @r - read file
- * @w - write file
-*/
-
 char buffer[1024];
 if (buffer == NULL)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
-}
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	exit(99);
 
 if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
 
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
 
 file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (file_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(file_from), exit(99);
-}
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	close(file_from), exit(99);
 
 while (r == 1024)
 {
 r = read(file_from, buffer, 1024);
 if (r == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
+
 w = write(file_to, buffer, r);
 if (w < r)
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-}
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
 if (close(file_from) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
-
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 if (close(file_to) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
-
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 return (0);
+}
 }
