08b0f56a2fd8c7e9a9315b56164ae6b4ae62ed3a
Melvin Quashie <melvinquashie@outlook.com>
2023-08-22 05:20:04 +0000

    update

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef __MAIN__
-#define __MAIN__
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 #define EXIT_SUCCESS 0
 
@@ -23,7 +23,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int nrandom();
 
-
-#endif /* __MAIN__ */
+#endif
